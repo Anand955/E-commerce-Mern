b7fc55e0c4a8867683c385283bdd4f2eabcdac43
Anand955 <coderanand23@gmail.com>
2025-05-29 08:34:32 +0530

    test

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button className="text-red-400  hover:bg-red-500">Click me</Button>
+    </div>
+  );
+}
+
+export default App;
