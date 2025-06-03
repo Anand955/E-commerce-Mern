e4325d0b6be00be501f36efd530cb5f2708222fd
Anand955 <coderanand23@gmail.com>
2025-06-03 07:41:02 +0530

    test

--- client/src/components/auth/layout.jsx
+++ client/src/components/auth/layout.jsx
@@ -6,7 +6,7 @@ function AuthLayout() {
       <div className="hidden lg:flex items-center justify-center bg-black w-1/2 px-12">
         <div className="max-w-2xl space-y-6 text-center text-primary-foreground">
           <h1 className="text-4xl font-extrabold tracking-tight text-white">
-            Welcome to ECommerce Shopping
+            Welcome to ECommerce Shopping hello Anand
           </h1>
         </div>
       </div>
